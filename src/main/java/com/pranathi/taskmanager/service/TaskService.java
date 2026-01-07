ab957161c34d7c631831ef42779d8ebfce81f48d
pranathi vanga <vangapranathi@gmail.com>
2026-01-07 18:45:31 +0530

    Inject TaskRepository into TaskService using constructor injection

--- src/main/java/com/pranathi/taskmanager/service/TaskService.java
+++ src/main/java/com/pranathi/taskmanager/service/TaskService.java
@@ -1,7 +1,12 @@
 package com.pranathi.taskmanager.service;
 
+import com.pranathi.taskmanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository=taskRepository;
+    }
 }
