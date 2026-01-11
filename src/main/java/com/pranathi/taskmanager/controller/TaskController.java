de2b44c23ad8b88a33c31a8e26fa4e4576505c8a
pranathi vanga <vangapranathi@gmail.com>
2026-01-11 22:09:49 +0530

    Added POST /tasks API to create task

--- src/main/java/com/pranathi/taskmanager/controller/TaskController.java
+++ src/main/java/com/pranathi/taskmanager/controller/TaskController.java
@@ -5,21 +5,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.pranathi.taskmanager.dto.TaskCreateRequest;
+
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
     private final TaskService taskService;
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
-        
-        return ResponseEntity.ok("Task manager backend is working");
+    @PostMapping
+    public ResponseEntity<String> createTask(
+            @RequestBody TaskCreateRequest request) {
+
+        taskService.createTask(
+                request.getTitle(),
+                request.getDescription()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Task created successfully");
     }
-
-
 }
