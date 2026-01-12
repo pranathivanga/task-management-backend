7405be2c15eeb3bc84f5d720e3dde6140ad017ef
pranathi vanga <vangapranathi@gmail.com>
2026-01-12 19:46:26 +0530

    Added SLF4J logging in TaskService

--- src/main/java/com/pranathi/taskmanager/service/TaskService.java
+++ src/main/java/com/pranathi/taskmanager/service/TaskService.java
@@ -2,6 +2,8 @@ package com.pranathi.taskmanager.service;
 
 import com.pranathi.taskmanager.dto.TaskCreateRequest;
 import com.pranathi.taskmanager.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,13 +11,13 @@ import java.util.List;
 
 @Service
 public class TaskService {
-
+    private static final Logger logger= LoggerFactory.getLogger(TaskService.class);
     private final List<TaskCreateRequest> tasks = new ArrayList<>();
 
+
     public void createTask(String title, String description) {
-        if (title == null || title.isBlank()) {
-            throw new IllegalArgumentException("Task title cannot be empty");
-        }
+        logger.info("Creating task with title: {}", title);
+
 
         TaskCreateRequest task = new TaskCreateRequest();
         task.setTitle(title);
